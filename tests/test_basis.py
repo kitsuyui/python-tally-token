6e258c29f4316af41d7c44d811f0da236baf03bf
kitsuyui <kitsuyui@kitsuyui.com>
2023-07-15 09:47:44 +0900

    Accept custom length splitting and merging

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -1,4 +1,5 @@
-from tally_token import split_text, merge_text
+import random
+from tally_token import split_text, merge_text, split_bytes_into, merge_bytes_into
 
 
 def test_split_merge():
@@ -6,3 +7,30 @@ def test_split_merge():
     clear_text = "Hello World!"
     token1, token2 = split_text(clear_text)
     assert clear_text == merge_text([token1, token2])
+
+
+def test_split_into_one_and_merge():
+    """Test that split_text with into=1 returns the original text."""
+    clear_text = "Hello World!"
+    tokens = split_text(clear_text, into=1)
+    assert tokens == [b'Hello World!']
+    assert clear_text == merge_text(tokens)
+
+
+def test_split_into_3():
+    """Test that split_text accept into=3."""
+    clear_text = "Hello World!"
+    tokens = split_text(clear_text, into=3)
+    assert len(tokens) == 3
+    assert clear_text == merge_text(tokens)
+
+
+def test_merge_order():
+    """Test that merge_text accepts tokens in any order."""
+    clear_text = "Hello World!"
+    tokens = split_text(clear_text, into=10)
+    assert len(tokens) == 10
+    assert clear_text == merge_text(tokens)
+    for _ in range(10):
+        random.shuffle(tokens)
+        assert clear_text == merge_text(tokens)
